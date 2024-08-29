3af90498051c53fddc8e3d90ad10da493c71db86
Gudisa1 <gudisagebi1@gmail.com>
2024-08-29 04:47:37 -0700

    terraform for S3

--- s3/IAC/terraform/s3.tf
+++ s3/IAC/terraform/s3.tf
@@ -1,4 +1,6 @@
-resource "aws_s3_bucket" "Gudisa-Bucket" {
+resource "aws_s3_bucket" "Gudisa_Bucket" {
+  bucket = "gudisa-files-bucket"  # Replace with a globally unique bucket name
+  
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
